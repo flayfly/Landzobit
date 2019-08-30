ba4aa14c85354ab1b8727910bb9846a0cdfeaf5b
flayfly <fly@example.com>
2019-08-30 17:24:57 +0800

    %HEAD debug

--- main.ts
+++ main.ts
@@ -689,13 +689,13 @@ namespace LANDZO_TS {
     }
     
     //% blockId=landzobit_desk_lamp_turn_on block="台灯开"
-    //% weight=101
+    //% weight=100
     export function DeskLampTurnOn(): void {
 
     }
 
     //% blockId=landzobit_desk_lamp_turn_off block="台灯灭"
-    //% weight=105
+    //% weight=100
     export function DeskLampTurnOff(): void {
 
     }
